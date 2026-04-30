d1152592e599349ecf55eac8fc6a592aa3a09bda
arifislam121416 <ardairyhelthcare@gmail.com>
2026-04-30 12:23:26 +0600

    SOME Add

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,8 +2,8 @@ import { mongodbAdapter } from "@better-auth/mongo-adapter";
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 
-const client = new MongoClient("mongodb://localhost:27017/database");
-const db = client.db();
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db("pixen");
 
 export const auth = betterAuth({
     emailAndPassword: { 
